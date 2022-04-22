a4305bda9f551d237031e23bd12a072d64403a2d
ngjorloff <niklas.gjorloff@gmail.com>
2022-04-23 00:13:09 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -119,7 +119,7 @@ configs.setup {
   }
 }
 
--- nvim-cmp.
+-- nvim-cmp
 local cmp = require'cmp'
 
 cmp.setup({
@@ -161,48 +161,48 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 
 -- KEYMAPPINGS
 local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true }
+local noremap = { noremap = true }
 local silent = { silent = true }
 
 -- Set leader key to space
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>", noremap)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Ctrl + s to save
-keymap('n', '<c-s>', ':w<CR>', {})
-keymap('i', '<c-s>', '<Esc>:w<CR>a', {})
+keymap('n', '<c-s>', ':w<CR>', noremap)
+keymap('i', '<c-s>', '<Esc>:w<CR>a', noremap)
 
 -- Exit with q
 keymap('n', '<leader>q', ':q<CR>', silent)
 
 -- Make escape do nothing when in insert mode
-keymap("i", "<Esc>", "<Nop>", opts)
+keymap("i", "<Esc>", "<Nop>", noremap)
 
 -- Use jj instead of escape to exit normal mode
-keymap('i', 'jj', '<Esc>', opts)
+keymap('i', 'jj', '<Esc>', noremap)
 
 -- Creating splits
 keymap('n', '<leader>v', ':vsplit<space>', {})
 keymap('n', '<leader>h', ':split<space>', {})
 
 -- Moving between splits
-keymap('n', '<c-j>', '<c-w>j', opts)
-keymap('n', '<c-k>', '<c-w>k', opts)
-keymap('n', '<c-h>', '<c-w>h', opts)
-keymap('n', '<c-l>', '<c-w>l', opts)
+keymap('n', '<c-j>', '<c-w>j', noremap)
+keymap('n', '<c-k>', '<c-w>k', noremap)
+keymap('n', '<c-h>', '<c-w>h', noremap)
+keymap('n', '<c-l>', '<c-w>l', noremap)
 
 -- Resize splits
-keymap('n', '<up>', '<c-w>+', opts)
-keymap('n', '<down>', '<c-w>-', opts)
-keymap('n', '<left>', '3<c-w><', opts)
-keymap('n', '<right>', '3<c-w>>', opts)
+keymap('n', '<up>', '<c-w>+', noremap)
+keymap('n', '<down>', '<c-w>-', noremap)
+keymap('n', '<left>', '3<c-w><', noremap)
+keymap('n', '<right>', '3<c-w>>', noremap)
 
 -- Hide search results
 keymap('n', '<leader>n', ':nohl<CR>', silent)
 
 -- Telescope
-keymap('n', '<leader>f', '<cmd>:lua require("telescope.builtin").find_files()<cr>', opts)
-keymap('n', '<leader>g', '<cmd>:lua require("telescope.builtin").live_grep()<cr>', opts)
-keymap('n', '<leader>b', '<cmd>:lua require("telescope.builtin").buffers()<cr>', opts)
-keymap('n', '<leader>h', '<cmd>:lua require("telescope.builtin").help_tags()<cr>', opts)
+keymap('n', '<leader>f', '<cmd>:lua require("telescope.builtin").find_files()<cr>', noremap)
+keymap('n', '<leader>g', '<cmd>:lua require("telescope.builtin").live_grep()<cr>', noremap)
+keymap('n', '<leader>b', '<cmd>:lua require("telescope.builtin").buffers()<cr>', noremap)
+keymap('n', '<leader>h', '<cmd>:lua require("telescope.builtin").help_tags()<cr>', noremap)
