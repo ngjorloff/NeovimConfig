a570e4138ae47b8c930a8a74a1744b97fa4b3044
ngjorloff <niklas.gjorloff@gmail.com>
2022-04-22 20:57:55 +0200

    Fix console message

--- script.ps1
+++ script.ps1
@@ -5,7 +5,7 @@ if (!(Test-Path -PathType Container $neovimDir)) {
     New-Item -ItemType Directory -Force -Path $neovimDir | Out-Null
 }
 
-Write-Host "Copying .vim files to $neovimDir"
+Write-Host "Copying .lua files to $neovimDir"
 Copy-Item -Path ".\*" -Destination $neovimDir -Recurse -Include ".lua"
 
 & git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
